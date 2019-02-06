23d133cdc2d20d53b2aa842dc0a8ed3eb95556cb
Brandon Cao <brandon@chamberds.com>
2019-02-05 21:23:10 -0800

    scroll view for description controller open event url

--- InfoQuake/Controller/EarthquakeDescriptionController.swift
+++ InfoQuake/Controller/EarthquakeDescriptionController.swift
@@ -8,9 +8,16 @@
 
 import Foundation
 import UIKit
+import SafariServices
 
 class EarthquakeDescriptionController: BaseViewController {
+	private let leadingAnchorConst: CGFloat = 15.0
 	
+	private lazy var scrollView: UIScrollView = {
+		let v = UIScrollView()
+		v.translatesAutoresizingMaskIntoConstraints = false
+		return v
+	}()
 	
 	private lazy var titleLabel: UILabel = {
 		let l = UILabel()
@@ -22,6 +29,27 @@ class EarthquakeDescriptionController: BaseViewController {
 		return l
 	}()
 	
+	private lazy var placeLabel: UILabel = {
+		let l = UILabel()
+		l.translatesAutoresizingMaskIntoConstraints = false
+		l.textColor = Theme.Colors.cellFontColor.color
+		l.lineBreakMode = .byWordWrapping
+		l.numberOfLines = 0
+		l.font = UIFont.systemFont(ofSize: 20.0)
+		return l
+	}()
+	
+	private lazy var urlButton: UIButton = {
+		let b = UIButton()
+		b.translatesAutoresizingMaskIntoConstraints = false
+		b.setTitleColor(.blue, for: .normal)
+		b.setTitle("Link to event", for: .normal)
+		b.titleLabel?.font = UIFont.systemFont(ofSize: 17.0)
+		b.addTarget(self, action: #selector(openEventUrl), for: .touchUpInside)
+		return b
+	}()
+	
+
 	private var viewModel: EarthquakeDescViewModel!
 	
 	init(feature: Feature) {
@@ -36,13 +64,54 @@ class EarthquakeDescriptionController: BaseViewController {
 	override func setupController() {
 		super.setupController()
 		title = "Earthquake Details"
-		view.addSubview(titleLabel)
-		titleLabel.text = viewModel.title
+		view.addSubview(scrollView)
+		
+		// constrain the scroll view to 8-pts on each side
+		scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+		scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8.0).isActive = true
+		scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+		scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8.0).isActive = true
+		
+		scrollView.addSubview(titleLabel)
+		scrollView.addSubview(placeLabel)
+		scrollView.addSubview(urlButton)
+		
+
 		NSLayoutConstraint.activate([
-			titleLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 15),
-			titleLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 15),
-			titleLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -15),
+			titleLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 15),
+			titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: leadingAnchorConst),
+			titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -leadingAnchorConst),
+			placeLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 45),
+			placeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: leadingAnchorConst),
+
+			urlButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: leadingAnchorConst),
+			urlButton.topAnchor.constraint(equalTo: placeLabel.bottomAnchor, constant: 200),
+			urlButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -15),
+
+
 		])
+		
+		
+		titleLabel.text = viewModel.title
+		placeLabel.text = viewModel.place
+		
+	}
+	
+	@objc
+	private func openEventUrl() {
+		let safariVC = SFSafariViewController(url: URL(string: viewModel.url!)!)
+		safariVC.delegate = self
+		present(safariVC, animated: true, completion: nil)
+	}
+	
+}
+
+// MARK: - Safari View Controller Delegate
+extension EarthquakeDescriptionController: SFSafariViewControllerDelegate {
+
+	
+	func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+		controller.dismiss(animated: true, completion: nil)
 	}
 	
 }
